d918755f91cb90b9f06326c45f2074ce4d7b97c8
nicolas <nicolas-villela@hotmail.com>
2025-10-18 15:25:23 -0300

    Primeiro commit limpo

---
+++ script.js
@@ -0,0 +1,15 @@
+// JavaScript para o menu mobile
+const mobileMenuButton = document.getElementById('mobile-menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
+
+mobileMenuButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+});
+
+// Fechar o menu mobile ao clicar em um link
+const mobileMenuLinks = mobileMenu.querySelectorAll('a');
+mobileMenuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        mobileMenu.classList.add('hidden');
+    });
+});
