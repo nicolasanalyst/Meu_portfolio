777bb80d68f1341e75ed33c4df24ddad2ca6edb7
Nicolas Villela <105022587+nicolasanalyst@users.noreply.github.com>
2025-11-07 21:49:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// JavaScript para o menu mobile
 const mobileMenuButton = document.getElementById('mobile-menu-button');
 const mobileMenu = document.getElementById('mobile-menu');
 
@@ -6,7 +5,6 @@ mobileMenuButton.addEventListener('click', () => {
     mobileMenu.classList.toggle('hidden');
 });
 
-// Fechar o menu mobile ao clicar em um link
 const mobileMenuLinks = mobileMenu.querySelectorAll('a');
 mobileMenuLinks.forEach(link => {
     link.addEventListener('click', () => {
